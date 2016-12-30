b2f3edf12f077406a65e6cfb74e2f3198305f599
gd602546628 <602546628@qq.com>
2016-12-30 17:55:46 +0800

    fff

--- gQuery.js
+++ gQuery.js
@@ -109,10 +109,170 @@
                 callBack(name,obj[name]);
             }
         }
-    }
+    };
 
-    gQuery.extend=function(){
-    }
+    gQuery.extend=gQuery.fn.extend=function(){
+        var options,copy,clone,src,name, i,length,copyisArray,target,deep;
+        length=arguments.length;
+        i=1;
+        deep=false;
+        target=arguments[0];
+
+        if(gQuery.type(target) === "boolean"){
+            deep = target;
+            target = arguments[1] || {};
+            i=2;
+        };
+
+        if(length == i){
+            target = this;
+            --i;
+        };
+
+        for(;i<length;i++){
+            if((options = arguments[i]) != null){
+                 for(name in options){
+                     src=target[name];
+                     copy=options[name];
+
+                     if(src === copy ){
+                         continue
+                     };
+
+                     if(deep && (gQuery.isPlainObject(copy) || (copyisArray=gQuery.isArray(copy)))){
+                          if(copyisArray){
+                              copyisArray=false;
+                              clone = src && gQuery.isArray(src) ? src :[] ;
+                          }else{
+                              clone = src && gQuery.isPlainObject(src) ? src : {};
+                          };
+
+                         target[name] = gQuery.extend(deep,clone,copy);
+
+                     }else{
+                         target[name]=copy
+                     }
+                 }
+            }
+        };
+
+        return target
+    };
+
+    gQuery.CallBacks=function(options){
+        var defaults={
+            once:false,//是否只执行一次
+            memory:false,//是否记忆，启用将触发所有回调，包含后面加入的
+            only:false,//同一回调是否只添加一次
+            stopOnFalse:false//某一回调返回false，是否停止后续回调
+        };
+        defaults=gQuery.extend(true,defaults,options);
+
+        var self={},list=[],firing,fired,fireStart,firingLength,firingIndex,stack = !defaults.once && [],memory;
+
+        var fire=function(data){
+            memory= defaults.memory && data;
+            fired=true;
+            firingIndex=fireStart || 0;
+            fireStart=0;
+            firingLength=list.length;
+            firing=true;
+            for(;firingIndex<firingLength;firingIndex++){
+                if(list[firingIndex].apply(data[0],data[1]) == false && defaults.stopOnFalse){
+                     memory = false;
+                     break;
+                }
+            };
+
+            firing = false ;
+
+            if(stack){
+                if(stack.length){
+                    fire(stack.shift());
+                }
+            }else if(memory){
+                list=[];
+            }else{
+                list=null;
+                memory=false;
+            }
+
+        };
+
+        self={
+
+            add:function(){
+              if(list){
+                  var start=list.length;
+
+                  (function add(args){
+                        gQuery.each(args,function(i,arg){
+                            var type=gQuery.type(arg);
+                            if(type == "function"){
+                                if( !defaults.only || !self.has(arg)){
+                                     list.push(arg);
+                                }
+                            }else{
+                                    if(arg && arg.length && type!="string"){
+                                        add(arg);
+                                    }
+                            };
+                        });
+
+                      if(firing){
+                          firingLength=list.length;
+
+                      }else if(memory){
+                          fireStart=start;
+                          fire(memory)
+                      }
+
+                  })(arguments)
+              }
+                return this;
+            },
+
+            fire:function(){
+                self.fireWith(this,arguments);
+                return this;
+            },
+
+            fireWith:function(context,args){
+                args=args || [];
+                args=[ context, args.slice ? args.slice() : args ];
+                if(list && (!fired || stack)){
+                    if(firing){
+                        stack.push(args);
+                    }else{
+                        fire(args)
+                    }
+                };
+
+                return this;
+            },
+
+            has:function(obj){
+                return gQuery.inArray(obj,list) != -1;
+            }
+        }
+
+        return self;
+    };
+
+    gQuery.Deferred=function(){
+         var deferred={};
+         var  CallBack=gQuery.CallBacks({memory:true,once:true});
+        deferred.resolve=function(){
+            CallBack.fire();
+        };
+        deferred.done=function(fn){
+            CallBack.add(fn)
+        };
+
+        return deferred;
+    };
+
+
 
     window.gQuery=window.g=gQuery;
 })(window)
